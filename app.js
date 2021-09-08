35b22c741abfcf5ea98dd947267affcba4e076df
Peter Alabi <peteralabi@Peters-MacBook.local>
2021-09-08 14:46:25 -0600

    Basic Authentication

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
-var promotionRouter = require('./routes/promotionRouter');
+var promoRouter = require('./routes/promoRouter');
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
@@ -32,13 +32,39 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req,res,next){
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  if (!authHeader){
+    var err = new Error("You're not authenticated!");
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password'){
+    next();
+  }else{
+    var err = new Error("You're not authenticated!");
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
-app.use('/promotions', promotionRouter);
+app.use('/promotions', promoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/promoRouter.js
@@ -0,0 +1,93 @@
+const http = require('http');
+const express =require('express');
+const bodyParser = require('body-parser');
+const promotionRouter = express.Router();
+const mongoose = require('mongoose');
+const Promos = require('../models/promotions');
+
+const app = express();
+app.use(bodyParser.json());
+
+promotionRouter.route('/')
+.get((req,res,next) => {
+  Promos.find({})
+  .then((promos)=>{
+    res.statusCode = 200,
+    res.setHeader('Content-Type','application/json');
+    res.json(promos);
+  },(err)=>{
+    next(err);
+  })
+  .catch((err)=>next(err))
+})
+.post((req, res, next) => {
+  Promos.create(req.body)
+  .then((promo)=>{
+    console.log('Promo created',promo)
+    res.statusCode = 200,
+    res.setHeader('Content-Type','application/json');
+    res.json(promo);
+  },(err)=>{
+    next(err)
+  })
+  .catch((err)=>next(err))
+})
+.put((req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res, next) => {
+  Promos.remove({})
+  .then((promos)=>{
+    res.statusCode = 200,
+    res.setHeader('Content-Type','application/json');
+    res.json(promos);
+  },(err)=>{
+    next(err)
+  })
+  .catch((err)=>next(err))
+});
+
+
+promotionRouter.route('/:promotionId')
+.get((req,res,next) => {
+  Promos.findById(req.params.promotionId)
+  .then((promo)=>{
+    res.statusCode = 200,
+    res.setHeader('Content-Type','application/json');
+    res.json(promo);
+  },(err)=>{
+    next(err);
+  })
+  .catch((err)=>next(err))
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /promotions/'+ req.params.promotionId);
+})
+.put((req, res, next) => {
+  Promos.findByIdAndUpdate(req.params.promotionId,
+    {$set:req.body},
+    {new:true})
+  .then((promo)=>{
+    res.statusCode = 200,
+    res.setHeader('Content-Type','application/json');
+    res.json(promo);
+  },(err)=>{
+    next(err);
+  })
+  .catch((err)=>next(err))
+})
+.delete( (req, res, next) => {
+  Promos.findByIdAndRemove(req.params.promotionId)
+  .then((promo)=>{
+    res.statusCode = 200,
+    res.setHeader('Content-Type','application/json');
+    res.json(promo);
+  },(err)=>{
+    next(err);
+  })
+  .catch((err)=>next(err))
+});
+
+module.exports = promotionRouter;
