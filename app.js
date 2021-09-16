2a867c918a5c6bbe087093a2ccb2c3a6ad50a12d
peteralabi <peteralabi@Peters-MacBook.local>
2021-09-16 09:22:45 -0600

    File Upload

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+var uploadRouter = require('./routes/uploadRouter')
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
@@ -30,7 +31,14 @@ connect.then((db)=>{
 
 
 var app = express();
-
+app.all('*',(req,res,next)=>{
+  if(req.secure){
+    return next();
+  }
+  else{
+    res.redirect(307, 'https://'+ req.hostname + ':'+app.get('secPort')+req.url);
+  }
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -52,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
