18109af4cc1da3b166ec9b6445a7ce16936733cf
Peter Alabi <78501918+snipersenpai@users.noreply.github.com>
2021-10-24 22:22:31 -0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'secretKey':'12345-67890-09876-54321',
-  'mongoUrl':"mongodb://localhost:27017/conFusion",
-  'facebook':{
-    clientId: '252867313397919',
-    clientSecret:'9ccc5dd2ee3556dd433b15205bb1e818'
-  }
-}
